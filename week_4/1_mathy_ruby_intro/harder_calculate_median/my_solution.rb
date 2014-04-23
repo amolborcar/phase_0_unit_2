11c0cbff4e14d4950a7b7ba4e246c29272538511
Amol Borcar <aborcar@gmail.com>
2014-04-22 20:40:14 -0700

    Updated week 4 exercise 1
    Completed median challenge

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -18,30 +18,55 @@
 		# def median(array)
 	# Sort array in ascending order using array.sort
 	# If array is odd numbered, take element in array that is (array.length/2).floor	
-	# If array is even numberd, take average of elements in the middle of the array
+	# If array is even numbered, take average of elements in the middle of the array
 
 
 # 2. Initial Solution
 
 def median(array)
 	sorted = array.sort
-	# if array[0].is_a?(String)
-	# 	med = array[(array.length/2).floor]
-	# else
+
+	# Check if the array has string values by testing the first element
+
+	if sorted[0].is_a?(String)
+	med = sorted[(sorted.length/2).floor]
+	else
+
+	# Calculate median if there are no string values
 		if sorted.length%2 != 0
 			med = sorted[(sorted.length/2).floor]
 		else
 			med = (sorted[sorted.length/2]+sorted[(sorted.length/2)-1])/2.to_f
 		end
-	# else
-	# 	# Perform test on unsorted array
-	# end
+	end
 
 
 end
 
 # 3. Refactored Solution
 
+# Remove the test for string elements because the sort command already sorts strings
+
+def median(array)
+	sorted = array.sort
+
+	# Calculate median if there are no string values
+		if sorted.length%2 != 0
+			med = sorted[(sorted.length/2).floor]
+		else
+			med = (sorted[sorted.length/2]+sorted[(sorted.length/2)-1])/2.to_f
+		end
+
+end
 
 
 # 4. Reflection 
+
+=begin
+
+I found this challenge to be easier than the mode challenge.  My logic for calculating the median
+was correct and fairly easy to implement.  I didn't really need to try any new techniques, the only issues
+I had were getting the syntax exactly correct.  I feel pretty competent in everything I used in completing
+this challenge.	
+
+=end
