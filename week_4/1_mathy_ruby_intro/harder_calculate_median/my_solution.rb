7603a152528ccd85b3462aa14450fb6d342fbdbf
Amol Borcar <aborcar@gmail.com>
2014-04-22 19:04:23 -0700

    Completed some of challenge #1
    Completed easy, medium, and hard

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -9,14 +9,36 @@
 # 1. Pseudocode
 
 # What is the input?
+	# A single array
 # What is the output? (i.e. What should the code return?)
+	# The median or middle value of that array
 # What are the steps needed to solve the problem?
+
+	# Define method that takes one argument
+		# def median(array)
+	# Sort array in ascending order using array.sort
+	# If array is odd numbered, take element in array that is (array.length/2).floor	
+	# If array is even numberd, take average of elements in the middle of the array
 
 
 # 2. Initial Solution
 
+def median(array)
+	sorted = array.sort
+	# if array[0].is_a?(String)
+	# 	med = array[(array.length/2).floor]
+	# else
+		if sorted.length%2 != 0
+			med = sorted[(sorted.length/2).floor]
+		else
+			med = (sorted[sorted.length/2]+sorted[(sorted.length/2)-1])/2.to_f
+		end
+	# else
+	# 	# Perform test on unsorted array
+	# end
 
 
+end
 
 # 3. Refactored Solution
 
--- week_4/2_creative/create_accountability_groups/my_solution.rb
+++ week_4/2_creative/create_accountability_groups/my_solution.rb
@@ -1,19 +1,67 @@
 # U2.W4: Create Accountability Groups
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: Single array of all student names in cohort
+# Output: Multiple arrays, each with 4 students per array
 # Steps:
+	# 1. Create array with all student names
+	# 2. Create a method that takes a single input, an array
+	# 3. Create method for randomizing which students are grouped with whom
+	# 4. Create method for splitting cohort into groups of 4
+	# 4. 
 
 
 # 3. Initial Solution
 
+# Create full cohort as an array
+
+cohort = ["Bridgette Salcido", "Matias Meneses", "Ariel Fogel", "RJ Bernaldo", "Jeuel Wilkerson", 
+"Amol Borcar", "Andrew Koines", "Kendall Carey", "Kris Shanks", "Alec Ashford", "Amelia Downs",
+"Ronald Ishak", "Gregory White", "Danny Glover", "Jeff Kynaston", "Josh Jeong", "Matthew Kuzio",
+"Kirstin Jarchow", "Phillip Crawford", "Austin Bourdier", "Ryan Stack", "Scott Jason",
+"Sherwood Callaway", "Val Sverdlov", "Sapan Bhuta", "Britney Valkenburg", "Brennon Williams"]
 
 
+def account(array)
+
+    # Create a loop to run through code three times, one for each unit
+    
+    x = 1
+
+    while x < 4 do
+        puts "Unit " + x.to_s + " Accountability Groups:"
+        puts "-------------------"
+    
+    # Randomnize the order of the cohort
+        scramble = array.shuffle
+	# Create an array out of each group of 4
+	    groups = scramble.each_slice(4).to_a
+	# Extract these groups into their own arrays
+	    i = 0
+        
+	    while i < groups.length do
+            if i == groups.length-1
+                puts groups[i]
+                puts "-------------------
+                "
+            else
+		        puts groups[i]
+                puts "----"
+            end
+		    i += 1
+	    end
+        
+        x += 1
+
+    end
+
+end
+
+account(cohort)
 
 # 4. Refactored Solution
 
