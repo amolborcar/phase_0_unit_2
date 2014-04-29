eb756ffdf6f0174abeb2d17fd8e9fb2aee2c5c0c
Amol Borcar <aborcar@gmail.com>
2014-04-28 20:22:06 -0700

    Completed week 5 challenge #2

--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -14,29 +14,65 @@
 
 class Die
   def initialize(labels)
+  	if labels.length == 0
+  		raise ArgumentError.new("You must have at least one label!")
+  	else
+      @labels = labels
+    end
   end
 
   def sides
+  	return @labels.length
   end
 
   def roll
+  	return @labels.sample
   end
 end
 
 
 
+
 # 4. Refactored Solution
 
+# I don't think there's anything to refactor
 
+class Die
+  def initialize(labels)
+  	if labels.length == 0
+  		raise ArgumentError.new("You must have at least one label!")
+  	else
+      @labels = labels
+    end
+  end
 
+  def sides
+  	return @labels.length
+  end
 
+  def roll
+  	return @labels.sample
+  end
+end
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+# Die class is initialized with a single argument
 
+Die.instance_method(:initialize).arity.should == 1
 
+# Sides method returns the length of the array
 
+Die.new(["a","b","c","d","e"]).sides == 5
 
+# Roll method returns one of the inputs
+
+die = Die.new(["a","b","c","d","e"])
+die.roll == "a" || die.roll == "b" || die.roll == "c" || die.roll == "d" || die.roll == "e"
 
 # 5. Reflection 
+
+# This challenge was very similar to challenge 1 because of how I structured my roll method in 
+# the last challenge, so nothing new to report.  I still am having trouble writing driver code and am not entirely
+# sure the one I've written here is correct.
