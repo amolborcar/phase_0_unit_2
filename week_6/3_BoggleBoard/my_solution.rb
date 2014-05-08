ee03a8f1e4aa6149672d67ea16f026eb595942a6
Amol Borcar <aborcar@gmail.com>
2014-05-08 15:30:03 -0700

    finished challenge #3 and started second solo challenge

--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -9,51 +9,78 @@
 
 
 # 3. Initial Solution
-class BoggleBoard
-	
-	def initialize(grid)
-		@grid = grid
-	end
 
-	@new_array = Array.new()
-
-	def create_word(coords)
-		@coords = coords
-		@coords.each do |x|
-			@new_array.push(@grid[x[0]][x[1]])
-		end
-		# @coords.each do |x|
-		# 	print x[0]
-		# end
-		@new_array.join("")
-	end
-
-
-  #your code here
- 
-end
  
  
 dice_grid = [["b", "r", "a", "e"],
              ["i", "o", "d", "t"],
              ["e", "c", "l", "r"],
              ["t", "a", "k", "e"]]
- 
-boggle_board = BoggleBoard.new(dice_grid)
-boggle_board.create_word([[1,2],[1,1],[2,1],[3,2]])
- 
 
 
 # 4. Refactored Solution
+# I don't see anything to refactor...
 
 
+class BoggleBoard
 
+  #your code here
+	
+	def initialize(grid)
+		@grid = grid
+	end
+
+	def create_word(coords)
+		@word_array = []
+
+		coords.each do |x|
+			@word_array.push(@grid[x[0]][x[1]])
+		end
+		
+		p @word_array.join("")
+
+	end
+
+	def get_row(row)
+		return @grid[row]
+	end
+
+	def get_column(col)
+        @col_array = []
+        @grid.each do |row|
+            @col_array.push(row[col])
+        end
+        return @col_array
+    end
+
+ 
+end
+
+ 
+boggle_board = BoggleBoard.new(dice_grid)
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # create driver test code to retrieve a value at a coordinate here:
+
+def assert
+	raise "Wrong result or run error!" unless yield
+end
+
 # implement tests for each of the methods here:
+
+assert { boggle_board.create_word([[1,2],[1,1],[2,1],[3,2]]) == "dock" }
+assert { boggle_board.get_row(1) == ['i','o','d','t'] }
+assert { boggle_board.get_column(1) == ['r','o','c','a'] }
 
 
 # 5. Reflection 
+
+# I had to sleep on this challenge because I couldn't get it to work correctly at first, but when I returned
+# it was just a simple syntax error.  I think my final answer to this challenge was very similar
+# to my original Boggle Board challenge, because in that challenge (which I paired on) we created
+# the boggle board as its own class.  So except for the new word method, everything here was very similar.
+# I'm not sure if that is how it was supposed to work.  Either way, I didn't find this too challenging after
+# I took a step back and tried it again.  I think this challenge did help reinforce how I work best, though,
+# and I'm glad I didn't just spin my wheels for a while trying to figure it out.
--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -6,12 +6,78 @@
 
 # Pseudocode
 
+# Create a Song class that accepts two arguments, name and artist
+	# Create a method play to play a song
+
+# Create a Playlist class that requires any number of arguments as songs
+	# Create a method called add that adds a Song object
+	# Create a method called num_of_tracks that counts the songs in the playlist
+	# Create a method called remove that deletes a Song object
+	# Create a method called play_all that puts all songs in the playlist
+	# create a method called display that lists all songs in the playlist
+
 
 
 # Initial Solution
 
+class Song
+	
+	def initialize(name, artist)
+		@name = name
+		@artist = artist
+	end
 
+	attr_reader :name
 
+	def play
+		puts "Now playing: " + @name
+	end
+
+end
+
+class Playlist
+	
+	def initialize(*songs)
+		@playlist_songs = []
+		songs.each do |x|
+			@playlist_songs << x
+		end
+	end
+
+	def add(*new_songs)
+		@new_songs = []
+		new_songs.each do |x|
+			@playlist_songs << x
+		end
+		
+	end
+
+	def remove(*removed_songs)
+		@removed_songs = []
+		removed_songs.each do |x|
+			@removed_songs << x
+		end
+		@removed_songs.each do |x|
+			@playlist_songs.delete_at(@playlist_songs.index(x))
+		end
+		
+	end
+	
+	def num_of_tracks
+		return @playlist_songs.length
+	end
+
+	def play_all
+		puts "Playing the entire playlist:"
+		@playlist_songs.each { |x| puts "Playing: " + x.name }
+	end
+
+	def display
+		puts "The following songs are in the playlist:"
+		@playlist_songs.each { |x| puts x.name }
+	end
+
+end
 
 # Refactored Solution
 
@@ -31,15 +97,14 @@ lying_from_you = Song.new("Lying From You", "Linkin Park")
 angels = Song.new("Angels", "Within Temptation")
  
 my_playlist.add(lying_from_you, angels)
+my_playlist.display
+
 p my_playlist.num_of_tracks == 5
 going_under.play
 my_playlist.remove(angels)
-p my_playlist.includes?(lying_from_you) == true
+p my_playlist.include?(lying_from_you) == true # -- this line is still giving problems...
 my_playlist.play_all
 my_playlist.display
 
 
-
-
-
-# Reflection 
+# # Reflection 
