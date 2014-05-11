db0bf357cd209dd275654ec5cae25dcf2bed2189
Amol Borcar <aborcar@gmail.com>
2014-05-11 16:14:05 -0700

    Completed week 6 challenge 5 and bonus challenge

--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -1,26 +1,94 @@
 # U2.W6: Refactoring for Code Readability
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge with:
+
+1. Amol Borcar
+2. Alec Ashford
 
 
 # Original Solution
+
+class CreditCard
+	def initialize(card_number)
+		@card_number = card_number.to_s.split("")
+		raise ArgumentError.new("Must be exactly 16 digits!") if @card_number.length != 16 
+	end 
+
+	def double
+		rcard = @card_number.reverse.map {|i| i.to_i}
+		doubled = rcard.each_index.map do |i|
+			if i % 2 == 0
+				rcard[i] 
+			else
+				rcard[i] * 2
+			end
+		end
+		return doubled
+	end
+
+	def split_sum
+		total = 0 
+		double.map do |i|
+			if i.to_s.length > 1
+				double_digit = i.to_s.split("")
+				total += (double_digit[0].to_i + double_digit[1].to_i)
+
+			else
+				total += i
+			end
+		end
+		return total
+	end
+
+	def check_card
+		if split_sum % 10 == 0 
+			true
+		else
+			false 
+		end
+	end
+end
+
+# 1. DRIVER TESTS GO BELOW THIS LINE
+
 
 
 
 
 # Refactored Solution
 
+class CreditCard
+	def initialize(card_number)
+		  @card_number = card_number.to_s.split("")
+		  raise ArgumentError.new("Must be exactly 16 digits!") if @card_number.length != 16 
+	end 
 
-
-
-
+    def check_card
+      @card_number = @card_number.map {|i| i.to_i}
+      @card_number = @card_number.each_index.map do |i|
+		    if i % 2 == 1
+			    @card_number[i]
+		    else
+			    @card_number[i] * 2
+		    end
+		  end
+		  @card_number.join("").split("").map { |i| i.to_i }.reduce(:+) % 10 == 0
+    end
+end
 
 # DRIVER TESTS GO BELOW THIS LINE
 
-
-
-
+card = CreditCard.new(4563960122001999)
+puts card
 
 
 # Reflection 
+
+# I enjoyed this solution.  We encountered one complication in that the first code we looked at was already too
+# clean to refactor, so we needed to poke around for another code.  After that, we decided to condense
+# everything into one method and then worked line by line to get the code to do what we wanted it to do.
+# I think we were mostly on the same page and were able to solve all of our minor problems that we encountered.
+# Overall, though, I think refactoring a challenge that we had already refactored once (in week 5) was a little
+# redundant, since we both had our own experience to draw from, so maybe a completely new code to refactor, 
+# as we had in the GPS, would have been a little more challenging.
--- week_6/8_BONUS_CarClass/my_solution.rb
+++ week_6/8_BONUS_CarClass/my_solution.rb
@@ -1,15 +1,101 @@
 # U2.W6: Create a Car Class from User Stories
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
+
+# USER STORIES
+
+# As a video game player, I want to be able to create a new car and set it's model and color so I 
+# can customize it in the video game.
+# As a video game player, I need to be able to define a distance to drive so I can follow 
+# directions appropriately.
+# As a video game player, I'd like to be able to see the speed I am traveling, so I can properly 
+# accelerate or decelerate.
+# As a video game player, I want to be able to turn left or right so I can navigate a city and 
+# follow directions.
+# As a video game player, I want to be able to accelerate and decelerate the car to a defined speed 
+# so I can drive following the rules of the road.
+# As a video game player, I want to keep track of the total distance I have travelled, so I can 
+# get paid for mileage.
+# As a video game player, I want to be able to stop the car so I can follow traffic signs and 
+# signals.
+# As a video game player, I would like to see output reflecting the latest action of my car so 
+# I can see a play-by-play of the pizza delivery.
 
 
 # 2. Pseudocode
+
+# Create Car class
+	# Initialize class with model and color
+	# Create drive method with one parameter, distance
+	# Create view speed method
+	# Create turn method with one parameter (right vs left)
+	# Create change_speed method to accelerate/decelerate
+	# Create method to track speed
+		# This should create be tracked in an instance variable
+	# Create method to stop
+		# This changes speed to 0
+	# Each of these methods should puts what they are doing
+# End class
 
 
 
 # 3. Initial Solution
 
+class Car
+
+	@@distance = 0
+	
+	def initialize(model, color)
+		@model = model
+		@color = color
+		@speed = 0
+	end
+
+	# This should be modified to test if the speed variable is numeric 
+	def drive(distance)
+		if @speed == 0
+			puts "Please enter the speed you would like to go in MPH."
+			@speed = gets.chomp
+			puts "You drove " + distance.to_s + " miles at " + @speed.to_s + " MPH."
+		else
+			puts "You drove " + distance.to_s + " miles at your current speed."
+		end
+		@@distance += distance
+	end
+
+	# This could be modified to give the user choices, right or left
+
+	def turn(direction)
+		puts "You turned " + direction + "."
+	end
+
+	def stop
+		@speed = 0
+		puts "You have stopped. Your speed is now " + @speed.to_s + " MPH."
+	end
+
+	def change_speed(new_speed)
+
+		if new_speed.to_i > @speed.to_i
+			str = "You accelerated to "
+		else
+			str = "You decelerated to "
+		end
+
+		@speed = new_speed
+		puts str + @speed.to_s + " MPH."
+	end
+
+	def check_speed
+		puts "Your speed is " + @speed.to_s+ " MPH."
+	end
+
+	def check_distance
+		puts "Your total distance traveled was " + @@distance.to_s + "."
+	end
+
+end
 
 
 
@@ -22,9 +108,25 @@
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-
-
-
+my_car = Car.new("Tesla", "blue")
+my_car.drive(0.25)
+my_car.stop
+my_car.turn("right")
+my_car.drive(1.5)
+my_car.check_speed
+my_car.change_speed(15)
+my_car.drive(0.25)
+my_car.stop
+my_car.turn("left")
+my_car.check_speed
+my_car.drive(1.4)
+my_car.stop
+my_car.check_distance
 
 
 # 5. Reflection 
+
+# I'm still working on adding some things to this challenge but I enjoyed working on modifying
+# the methods to make it more user friendly.  I think conceptually this was pretty simple but 
+# actually coding it up was more challenging than I thought it would be, which I imagine will
+# emulate many of the coding challenges I'll see in the future.
